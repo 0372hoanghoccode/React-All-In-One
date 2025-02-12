5afe1bb14e6562e35306a938bcb3afb33f702f52
0372hoanghoccode <146009735+0372hoanghoccode@users.noreply.github.com>
2025-02-12 22:48:09 +0700

    done set shadcn with tailwind v4 react ts vite

---
+++ shadcn/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className='text-4xl text-center text-blue-500'>
+      hello world
+    <Button variant="outline" className="text-red-200 w-7 h7-">click </Button>
+    </div>
+
+  )
+}
